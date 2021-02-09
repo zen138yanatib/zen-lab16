7b1ede82cbf4ef4c2eb32162eb67a1c7c59c0a49
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-02-09 16:38:17 +0700

    Update lab16_2.cpp

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -1,19 +1,19 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char*& p , int N){
+	p = new char[N+1];
+	for(int i = 0; i < N;i++){
+	p[i] = 'A'+i;
+    p[N] = 0; 
+    }
 }
 
 int main(){
-	char *p;
-	int N;	
-	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
-	return 0;
+    char *p;
+    myString(p,5);
+    cout << p;	
+    delete [] p;
+    return 0;
 }
